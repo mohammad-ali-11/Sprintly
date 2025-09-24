58dea2f6206fb872a6ba7db0986aa67707d01da0
mohammad-ali-11 <mohammadalikhosravi78@gmail.com>
2025-09-24 16:00:25 +0330

    create project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+import "./App.css";
+
+export default function App(): ReactNode {
+  return <div className="app">hello frenda</div>;
+}
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+import path from "node:path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      // babel: {
+      //   plugins: ["babel-plugin-react-compiler"],
+      // },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
