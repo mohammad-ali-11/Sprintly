873eb62dc42077c5fd5bb1b576c2fa70c7a9c95a
mohammad-ali-11 <mohammadalikhosravi78@gmail.com>
2025-09-24 16:10:08 +0330

    change type file app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import type { ReactNode } from "react";
-
+import type { JSX } from "react";
 import "./App.css";
 
-export default function App(): ReactNode {
+export default function App(): JSXلهف ی.Element {
   return <div className="app">hello frenda</div>;
 }
