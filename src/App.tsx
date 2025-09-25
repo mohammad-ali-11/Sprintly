5a9e6ad8bdf43ce0cef0d2d61bddd2dc0fa6a0aa
mohammad-ali-11 <mohammadalikhosravi78@gmail.com>
2025-09-25 16:21:58 +0330

    add style and ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,74 @@
-import type { JSX } from "react";
-import "./App.css";
+import type { ReactNode } from "react";
 
-export default function App(): JSX.Element {
-  return <div className="app">hello frenda</div>;
+
+import styles from "./App.module.css";
+import clsx from "clsx";
+
+export default function App(): ReactNode {
+  return <div className={styles.app}>
+  <header>header</header>
+ <main>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Boards</h1> {/* اصلاح titel به title */}
+        <button >create</button>
+      </div>
+      <ul className={styles.boards}>
+        <li>
+          <div className={clsx(styles.board,'blue')}>
+            <div className={styles.cover}></div>
+            <div className={styles.content}>
+              <div className={styles.header}>
+                <div className={styles.title}>Board1</div> {/* اصلاح titel به title */}
+                <a href="./board">view</a>
+              </div>
+               <p className={styles.description}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi veniam ipsa
+              quidem quia beatae harum voluptate aperiam qui temporibus ab officiis sunt
+              aliquam voluptatibus, non consequuntur obcaecati atque. Non, dolorem?
+            </p>
+            </div>
+           
+          </div>
+        </li>
+        <li>
+          <div className={clsx(styles.board,'gray')}>
+            <div className={styles.cover}></div>
+            <div className={styles.content}>
+              <div className={styles.header}>
+                <div className={styles.title}>Board2</div> {/* اصلاح titel به title */}
+                <a href="./board">view</a>
+              </div>
+              <p className={styles.description}>
+              Adipisicing elit. Quasi veniam ipsa quidem quia beatae harum voluptate
+              aperiam qui temporibus ab officiis sunt aliquam voluptatibus, non consequuntur
+              obcaecati atque. Non, dolorem?
+            </p>
+            </div>
+            
+          </div>
+        </li>
+        <li>
+          <div className={ clsx(styles.board,'yellow')}>
+            <div className={styles.cover}></div>
+            <div className={styles.content}>
+              <div className={styles.header}>
+                <div className={styles.title}>Board3</div> {/* اصلاح titel به title */}
+                <a href="./board">view</a>
+              </div>
+               <p className={styles.description}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi veniam ipsa
+              quidem quia beatae harum voluptate aperiam qui temporibus ab officiis sunt
+              aliquam voluptatibus, non consequuntur obcaecati atque. Non, dolorem?
+            </p>
+            </div>
+           
+          </div>
+        </li>
+      </ul>
+      <footer className={styles.footer}>footer</footer>
+    </main>
+
+  <footer>footer</footer>
+    </div>
+ 
 }
