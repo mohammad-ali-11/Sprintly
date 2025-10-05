eeb07fa857667d7485f9e148ca629a1303c3c7fc
mohammad-ali-11 <mohammadalikhosravi78@gmail.com>
2025-10-05 15:17:26 +0330

    refactor BoardList

--- src/components/Board/components/BoardLists/BoardLists.tsx
+++ src/components/Board/components/BoardLists/BoardLists.tsx
@@ -5,68 +5,28 @@ import List from "@/components/list/list";
 import type { ListType } from "@/types/list";
 
 import styles from "./BoardLists.module.css";
+import { listData } from "@/data/list-data";
 
 export default function BoardLists(): ReactNode {
-  const [todoList, setTodoList] = useState<ListType>({
-    id: "1",
-    title: "🔜 To Do",
-    items: [
-      {
-        id: "item-1",
-        title: "Implement Landing Page",
-      },
-      {
-        id: "item-2",
-        title: "Amplement Search Page",
-      },
-      {
-        id: "item-3",
-        title: "Navbar Component",
-      },
-    ],
-  });
-  const [doingList] = useState<ListType>({
-    id: "1",
-    title: "🔨 Doing",
-    items: [
-      {
-        id: "item-1",
-        title: "hello Search Page",
-      },
-      {
-        id: "item-2",
-        title: "bavbar Component",
-      },
-    ],
-  });
-  // const handeldeleteItemClick=()=>{
-  //   setTodoList(...todoList)
-  //   const clone={...todoList,items:[...todoList,todoList.id]}
-  //   console.log(clone);
-
-  // }
-  // const handelListItemClick=useMemo(()=>{
-  //  return (id:string)=>{
-  // setTodoList((old)=>{
-  //   const clone=[...old.items]
-  //   return {...old,items:clone.filter((item)=>item.id!=id)}
-  // })
-  // }
-  // },[])
-  const handelListItemClick = useCallback((id: string) => {
-    setTodoList((old) => {
-      const clone = [...old.items];
-      return { ...old, items: clone.filter((item) => item.id != id) };
+  const [list,setList]=useState<ListType[]>(listData)
+ 
+  const handelListItemClick = useCallback((id: string):void => {
+    setList((old) => {
+      const clone={ ...old[0], items:[...old[0].items].filter((item) => item.id != id) };
+      return [clone,old[1],old[2]]
     });
   }, []);
 
   return (
     <ul className={styles["board-lists"]}>
       <li>
-        <List list={todoList} onclick={handelListItemClick} />
+        <List list={list[0]} onclick={handelListItemClick} />
       </li>
       <li>
-        <List list={doingList} onclick={handelListItemClick} />
+        <List list={list[1]} onclick={handelListItemClick} />
+      </li>
+       <li>
+        <List list={list[2]} onclick={handelListItemClick} />
       </li>
     </ul>
   );
