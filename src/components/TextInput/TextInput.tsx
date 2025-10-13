5dd3da474dfbb54a999e11b1773b56f74d987b71
mohammad-ali-11 <mohammadalikhosravi78@gmail.com>
2025-10-13 07:50:15 +0330

    add style text input

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,20 @@
+import {useId, type ComponentProps, type ReactNode,} from "react";
+import clsx from "clsx";
+import styles from "./TextInput.module.css";
+
+type props = ComponentProps<"input"> & {
+  label: string;
+};
+export default function Modal({
+  className,
+  label,
+  ...otherProps
+}: props): ReactNode {
+  const id=useId()
+  return (
+    <div className={clsx(styles["text-input"], className)}>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} {...otherProps} />
+    </div>
+  );
+}
